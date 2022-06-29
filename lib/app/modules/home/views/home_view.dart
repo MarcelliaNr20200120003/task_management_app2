352086a8dd60c7181b7458669beba6f424ece808
MarcelliaNr <marcellianr1234@gmail.com>
2022-06-29 18:10:35 +0700

    sidebar

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'package:ionicons/ionicons.dart';
-import 'package:task_management_app2/app/routes/app_pages.dart';
+import 'package:task_management_app2/app/utils/widget/header.dart';
 import 'package:task_management_app2/app/utils/widget/sidebar.dart';
 
 import '../controllers/home_controller.dart';
@@ -21,9 +20,17 @@ class HomeView extends GetView<HomeController> {
             ),
             Expanded(
               flex: 15,
-              child: Container(
-                color: Colors.white,
-              ),
+              child: Column(children: [
+                const header(),
+                Expanded(
+                    child: Container(
+                  padding: const EdgeInsets.all(50),
+                  margin: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(50)),
+                ))
+              ]),
             ),
           ],
         ));
