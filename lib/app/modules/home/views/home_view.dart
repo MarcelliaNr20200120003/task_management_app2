f3e719388399f33419a101e82f9e5574ec6afe21
MarcelliaNr <marcellianr1234@gmail.com>
2022-07-01 21:07:49 +0700

    home dart

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -11,28 +11,29 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.blue[100],
-        body: Row(
-          children: [
-            const Expanded(
-              flex: 2,
-              child: SideBar(),
-            ),
-            Expanded(
-              flex: 15,
-              child: Column(children: [
-                const header(),
-                Expanded(
-                    child: Container(
-                  padding: const EdgeInsets.all(50),
-                  margin: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(50)),
-                ))
-              ]),
-            ),
-          ],
-        ));
+      backgroundColor: Colors.blue[100],
+      body: Row(
+        children: [
+          const Expanded(
+            flex: 2,
+            child: SideBar(),
+          ),
+          Expanded(
+            flex: 15,
+            child: Column(children: [
+              const header(),
+              Expanded(
+                  child: Container(
+                padding: const EdgeInsets.all(50),
+                margin: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(50)),
+              ))
+            ]),
+          ),
+        ],
+      ),
+    );
   }
 }
