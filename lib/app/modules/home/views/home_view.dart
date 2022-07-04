9a2f715df977a1e03160367ce4b8b080eb74d057
MarcelliaNr <marcellianr1234@gmail.com>
2022-07-04 11:57:32 +0700

    home screen,responsive app

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:ionicons/ionicons.dart';
 import 'package:task_management_app2/app/utils/style/AppColors.dart';
 import 'package:task_management_app2/app/utils/widget/header.dart';
 import 'package:task_management_app2/app/utils/widget/sidebar.dart';
@@ -8,28 +9,92 @@ import 'package:task_management_app2/app/utils/widget/sidebar.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  const HomeView({Key? key}) : super(key: key);
+  final GlobalKey<ScaffoldState> _drawerKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _drawerKey,
+      drawer: const SideBar(),
       backgroundColor: AppColors.primaryBg,
       body: Row(
         children: [
-          const Expanded(
-            flex: 2,
-            child: SideBar(),
-          ),
+          !context.isPhone
+              ? const Expanded(
+                  flex: 2,
+                  child: SideBar(),
+                )
+              : const SizedBox(),
           Expanded(
             flex: 15,
             child: Column(children: [
-              const header(),
+              !context.isPhone
+                  ? const header()
+                  : Container(
+                      padding: const EdgeInsets.all(20),
+                      child: Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              _drawerKey.currentState!.openDrawer();
+                            },
+                            icon: const Icon(
+                              Icons.menu,
+                              color: AppColors.primaryText,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          Column(
+                            //mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Text(
+                                'Task Management',
+                                style: TextStyle(
+                                    fontSize: 20, color: AppColors.primaryText),
+                              ),
+                              Text(
+                                'Manage task made easy with friends',
+                                style: TextStyle(
+                                    fontSize: 15, color: AppColors.primaryText),
+                              ),
+                            ],
+                          ),
+                          const Spacer(),
+                          const Icon(
+                            Ionicons.notifications,
+                            color: AppColors.primaryText,
+                            size: 30,
+                          ),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(30),
+                            child: const CircleAvatar(
+                              backgroundColor: Colors.amber,
+                              radius: 25,
+                              foregroundImage: NetworkImage(
+                                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSibTMP3266859JrPVaoYmIXXinEtq9Bn_hfg&usqp=CAU'),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+              // content / isi page / screen
               Expanded(
                   child: Container(
                 padding: const EdgeInsets.all(50),
-                margin: const EdgeInsets.all(10),
+                margin: !context.isPhone
+                    ? const EdgeInsets.all(10)
+                    : const EdgeInsets.all(0),
                 decoration: BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.circular(50)),
+                    borderRadius: !context.isPhone
+                        ? BorderRadius.circular(50)
+                        : BorderRadius.circular(30)),
               ))
             ]),
           ),
