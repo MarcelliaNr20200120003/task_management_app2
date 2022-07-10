7d9e6ab59da860ef2c3eef4ddc564231dda2ab1f
MarcelliaNr <marcellianr1234@gmail.com>
2022-07-10 23:30:04 +0700

    Task Screen,BottomSheet

--- lib/app/modules/task/views/task_view.dart
+++ lib/app/modules/task/views/task_view.dart
@@ -85,20 +85,148 @@ class TaskView extends GetView<TaskController> {
                     ),
               // content / isi page / screen
               Expanded(
-                  child: Container(
-                padding: const EdgeInsets.all(50),
-                margin: !context.isPhone
-                    ? const EdgeInsets.all(10)
-                    : const EdgeInsets.all(0),
-                decoration: BoxDecoration(
+                child: Container(
+                  padding: !context.isPhone
+                      ? const EdgeInsets.all(40)
+                      : const EdgeInsets.all(20),
+                  margin: !context.isPhone
+                      ? const EdgeInsets.all(10)
+                      : const EdgeInsets.all(0),
+                  decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: !context.isPhone
-                        ? BorderRadius.circular(50)
-                        : BorderRadius.circular(30)),
-              ))
+                        ? BorderRadius.circular(40)
+                        : BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'My Task',
+                        style: TextStyle(
+                          color: AppColors.primaryText,
+                          fontSize: 30,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 7,
+                      ),
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: 8,
+                          clipBehavior: Clip.antiAlias,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              height: 150,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: AppColors.cardBg,
+                              ),
+                              margin: const EdgeInsets.all(10),
+                              padding: const EdgeInsets.all(20),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    children: [
+                                      ClipRRect(
+                                        borderRadius: BorderRadius.circular(25),
+                                        child: const CircleAvatar(
+                                          backgroundColor: Colors.amber,
+                                          radius: 20,
+                                          foregroundImage: NetworkImage(
+                                              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSibTMP3266859JrPVaoYmIXXinEtq9Bn_hfg&usqp=CAU'),
+                                        ),
+                                      ),
+                                      ClipRRect(
+                                        borderRadius: BorderRadius.circular(25),
+                                        child: const CircleAvatar(
+                                          backgroundColor: Colors.amber,
+                                          radius: 20,
+                                          foregroundImage: NetworkImage(
+                                              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSibTMP3266859JrPVaoYmIXXinEtq9Bn_hfg&usqp=CAU'),
+                                        ),
+                                      ),
+                                      const Spacer(),
+                                      Container(
+                                        height: 25,
+                                        width: 80,
+                                        color: AppColors.primaryBg,
+                                        child: const Center(
+                                            child: Text(
+                                          '100%',
+                                          style: TextStyle(
+                                            color: AppColors.primaryText,
+                                          ),
+                                        )),
+                                      ),
+                                    ],
+                                  ),
+                                  const Spacer(),
+                                  Container(
+                                    height: 25,
+                                    width: 80,
+                                    color: AppColors.primaryBg,
+                                    child: const Center(
+                                      child: Text(
+                                        '10 / 10 Task',
+                                        style: TextStyle(
+                                          color: AppColors.primaryText,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  const Text(
+                                    'Pemrograman Mobile',
+                                    style: TextStyle(
+                                      color: AppColors.primaryText,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  const Text(
+                                    'Deadline 2 hari lagi',
+                                    style: TextStyle(
+                                      color: AppColors.primaryText,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
             ]),
           ),
         ],
+      ),
+      floatingActionButton: Align(
+        alignment: Alignment(0.95, 0.95),
+        child: FloatingActionButton.extended(
+          onPressed: () {
+            Get.bottomSheet(
+              Container(
+                margin: !context.isPhone
+                    ? const EdgeInsets.only(right: 150, left: 150)
+                    : null,
+                height: Get.height,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30)),
+                  color: Colors.white,
+                ),
+              ),
+            );
+          },
+          label: Text('Add Task'),
+          icon: Icon(Ionicons.add),
+        ),
       ),
     );
   }
