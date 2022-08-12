70d69667fb4aea0991ecf0e7dcfd23d0c8d98e10
MarcelliaNr <marcellianr1234@gmail.com>
2022-08-13 00:16:57 +0700

    Config UI and Data for Responsive Web App

--- lib/app/utils/widget/profileWidget.dart
+++ lib/app/utils/widget/profileWidget.dart
@@ -1,12 +1,11 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:task_management_app2/app/utils/style/AppColors.dart';
 
-class ProfileW extends StatelessWidget {
-  const ProfileW({
-    Key? key,
-  }) : super(key: key);
+import '../../data/controller/auth_controller.dart';
+import '../style/AppColors.dart';
+
+class profileWidget extends StatelessWidget {
+  final authCon = Get.find<AuthController>();
 
   @override
   Widget build(BuildContext context) {
@@ -14,34 +13,36 @@ class ProfileW extends StatelessWidget {
       child: !context.isPhone
           ? Row(
               children: [
-                const Expanded(
+                Expanded(
                   flex: 1,
                   child: ClipRRect(
                     child: CircleAvatar(
                       backgroundColor: Colors.amber,
-                      radius: 90,
-                      foregroundImage: NetworkImage(
-                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSibTMP3266859JrPVaoYmIXXinEtq9Bn_hfg&usqp=CAU'),
+                      radius: 150,
+                      foregroundImage:
+                          NetworkImage(authCon.auth.currentUser!.photoURL!),
                     ),
                   ),
                 ),
-                // SizedBox( width: 10,),
+                // SizedBox(
+                //   width: 20,
+                // ),
                 Expanded(
                   flex: 2,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
+                    children: [
                       Text(
-                        'Marcellia Nara.B',
-                        style: TextStyle(
+                        authCon.auth.currentUser!.displayName!,
+                        style: const TextStyle(
                           color: AppColors.primaryText,
-                          fontSize: 30,
+                          fontSize: 35,
                         ),
                       ),
                       Text(
-                        'marcellia@gmail.com',
-                        style: TextStyle(
+                        authCon.auth.currentUser!.email!,
+                        style: const TextStyle(
                           color: AppColors.primaryText,
                           fontSize: 16,
                         ),
@@ -53,31 +54,31 @@ class ProfileW extends StatelessWidget {
             )
           : Center(
               child: Column(
-                children: const [
-                  //SizedBox(
-                  //height: 40,
-                  //),
-                  ClipRRect(
-                    child: CircleAvatar(
-                      backgroundColor: Colors.amber,
-                      radius: 80,
-                      foregroundImage: NetworkImage(
-                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSibTMP3266859JrPVaoYmIXXinEtq9Bn_hfg&usqp=CAU'),
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: ClipRRect(
+                      child: CircleAvatar(
+                        backgroundColor: Colors.amber,
+                        radius: 150,
+                        foregroundImage:
+                            NetworkImage(authCon.auth.currentUser!.photoURL!),
+                      ),
                     ),
                   ),
-                  // SizedBox(
-                  //  height: 20,
-                  // ),
+                  const SizedBox(
+                    height: 20,
+                  ),
                   Text(
-                    'Marcellia Nara.B',
-                    style: TextStyle(
+                    authCon.auth.currentUser!.displayName!,
+                    style: const TextStyle(
                       color: AppColors.primaryText,
-                      fontSize: 30,
+                      fontSize: 35,
                     ),
                   ),
                   Text(
-                    'marcellia@gmail.com',
-                    style: TextStyle(
+                    authCon.auth.currentUser!.email!,
+                    style: const TextStyle(
                       color: AppColors.primaryText,
                       fontSize: 16,
                     ),
